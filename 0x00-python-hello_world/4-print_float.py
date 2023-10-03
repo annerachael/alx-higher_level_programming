7aeb89aea0868fb2f4588fcc12271ccc2c08625c
annerachael <annerwenji@gmail.com>
2023-10-03 04:16:08 -0700

    Hellow World

---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+number = 3.14159
+print(f"Float:{number:.2f} \n")
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+str = "Holberton School"
+new_str = 3 * str
+
+print(new_str[:9])
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
