3d6e8ade7be7c677511f027ef28ce5655b194b85
annerachael <annerwenji@gmail.com>
2023-12-13 14:54:12 -0800

    SQL Intro

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Displays top 3 cities temperature during July and August ordered by temp DECS;
 SELECT `city`, MAX(`value`) AS `max_temp`
 FROM `temperature`
-WHERE `month` in (7,8)
+WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
 ORDER BY `max_temp` DESC
 LIMIT 3;
