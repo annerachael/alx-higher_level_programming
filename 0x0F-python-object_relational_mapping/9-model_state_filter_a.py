2bae77d88192069a48a855b29f5e676c0373c12e
annerachael <annerwenji@gmail.com>
2024-01-18 06:51:53 -0800

    sqlalchemy

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    state = session.query(State)\
+    for state in session.query(State)\
                         .filter(State.name.ilike('%a%'))\
                         .order_by(State.id):
         print("{}: {}".format(state.id, state.name))
