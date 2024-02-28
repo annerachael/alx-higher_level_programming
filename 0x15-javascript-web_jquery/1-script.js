af054d89bc47d857d0429429b1285471f0225974
annerachael <annerwenji@gmail.com>
2024-02-28 12:57:15 -0800

    javascript

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,3 +1,1 @@
-document.addEventListener('DOMContentLoaded', function(){
-	document.querySelector('header').style.color = '#FF0000';
-}
+$('header').css('color', '#FF0000');
--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,5 +1,3 @@
-document.addEventListener('DOMContentLoaded', function(){
-	document.querySelector('DIV#red_header').onclick = function(){
-        	document.querySelector('header').style.color = '#FF0000';
-	}
-}
+$('DIV#red_header').click(function () {
+  $('HEADER').css('color', '#FF0000');
+});
