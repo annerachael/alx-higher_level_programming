edb6203337f035b604e236915e22f625a6c22006
annerachael <annerwenji@gmail.com>
2024-02-28 08:56:27 -0800

    javascript

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+	document.querySelector('DIV#red_header').onclick = function(){
+        	document.querySelector('header').style.color = '#FF0000';
+	}
+}
